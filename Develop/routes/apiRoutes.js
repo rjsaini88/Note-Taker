693d467024f2b6a0f3814e83c228cd237b5dc8cb
Rj Saini <rsaini88@gmail.com>
2023-01-02 21:15:38 -0800

    added helpers uuid

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const fs = require("fs");
+// Helper method for generating unique ids
+const uuid = require("../helpers/uuid");
 
 router.get("/", (req, res) => {
   res.json(`${req.method} REQUEST RECEIVED`);
@@ -6,5 +9,7 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   res.json(`${req.method} REQUEST RECEIVED`);
 });
+
+//how to read and write from the database
 
 module.exports = router;
