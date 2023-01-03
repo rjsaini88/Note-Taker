ad3246711792c1588c31954c1d0e66cd32a47d79
Rj Saini <rsaini88@gmail.com>
2023-01-02 22:00:20 -0800

    Working on read and write file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -6,10 +6,17 @@ const uuid = require("../helpers/uuid");
 router.get("/", (req, res) => {
   res.json(`${req.method} REQUEST RECEIVED`);
 });
+//Class notes video review 1:59
 router.post("/", (req, res) => {
   res.json(`${req.method} REQUEST RECEIVED`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newReview = { title, text, review_id: uuid() };
+  }
 });
 
 //how to read and write from the database
 
+//Get existing reviews
+const existingReviews = fs.readFileSync("../db/db.json", "utf-8");
 module.exports = router;
