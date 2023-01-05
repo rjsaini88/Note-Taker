21d034ae89c6a56e85a5c96ce348ba69832f32db
Rj Saini <rsaini88@gmail.com>
2023-01-04 23:06:16 -0800

    added FS read and write

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,20 +3,45 @@ const fs = require("fs");
 // Helper method for generating unique ids
 const uuid = require("../helpers/uuid");
 
+
 router.get("/", (req, res) => {
   res.json(`${req.method} REQUEST RECEIVED`);
 });
-//Class notes video review 1:59
+
+
 router.post("/", (req, res) => {
   res.json(`${req.method} REQUEST RECEIVED`);
+  
   const { title, text } = req.body;
   if (title && text) {
-    const newReview = { title, text, review_id: uuid() };
-  }
-});
+    const newNote = { title, text, review_id: uuid() 
+    };
+  // }
+// });
 
 //how to read and write from the database
 
 //Get existing reviews
-const existingReviews = fs.readFileSync("../db/db.json", "utf-8");
+const existingNotes = JSON.parse(fs.readFileSync("../db/db.json", "utf-8"));
+
+existingNotes.push(newNote);
+
+fs.writeFileSync(`../db/db.json`, JSON.stringify(existingNotes), (err)=>
+err
+? console.error(err)
+: console.log(`Your note ${newNote.title} has been saved. `)
+);
+
+const response = {
+  status: 'success',
+  body: newNote,
+};
+
+console.log(response);
+res.status(201).json(response);
+      }
+});
+
+
+
 module.exports = router;
